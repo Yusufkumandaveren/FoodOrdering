88f250ca5f351ba94b8954c260d8ec9b915d26a6
Yusuf Kumandaveren <yusufkumandaveren233@gmail.com>
2025-07-15 22:55:58 +0300

    sign-in & sign-up pages

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,65 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import Colors from "@/constants/Colors";
+import Button from "@/components/Button";
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{ title: "Sign in", headerTitleAlign: "center" }}
+      />
+      <Text style={styles.textButton}>Email</Text>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="jon@gmail.com"
+        style={styles.input}
+      />
+      <Text style={styles.textButton}>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+        placeholder=""
+        secureTextEntry
+      />
+      <Button text="Sign in" />
+      <Link href={'/(auth)/sign-up'} style={styles.accountText} >Creat an account</Link>
+    </View>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+    borderWidth:1,
+    borderColor:'gray'
+  },
+  textButton: {
+    fontSize: 20,
+    fontWeight: "300",
+    color: 'grey',
+    marginVertical: 10,
+  },
+  accountText:{
+    alignSelf:'center',
+    color: Colors.light.tint,
+    fontWeight:'bold',
+    fontSize: 18,
+  },
+});
